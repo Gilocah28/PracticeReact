4c712e4886cd7105ee0342456fb69a129d9da638
Gibermar Regalado <gberxx@gmail.com>
2024-05-06 13:51:05 +0800

    done styling tutorial

--- src/Components/StylingComponents/StylingComponent.jsx
+++ src/Components/StylingComponents/StylingComponent.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
+import './navbar.css'
+
 const StylingComponent = () => {
   return (
     <>
-    <nav>
-        <div>
+    <nav className='navbar'>
+        <div className='logo'>
             <h2>LOGO</h2>
         </div>
-        <ul>
+        <ul className='navbar-item'>
             <li><a>Home</a></li>
             <li><a>Contact</a></li>
             <li><a>About</a></li>
