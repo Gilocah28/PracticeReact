f5a4fcdfe147e3bdfc2bdcfad954acb9ffe0008b
Gibermar Regalado <gberxx@gmail.com>
2024-05-08 15:05:36 +0800

    Done Post Axios

--- src/Components/axiosTutorial/AxiosComponents.jsx
+++ src/Components/axiosTutorial/AxiosComponents.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { fetchPosts } from "../../service/apiService";
 
 const AxiosComponents = () => {
-  return (
-    <div>AxiosComponents</div>
-  )
-}
+  const [posts, setPosts] = useState([]);
 
-export default AxiosComponents
+  useEffect(() => {
+    const getPosts = async () => {
+        const postData = await fetchPosts();
+        setPosts(postData);
+    }
+    getPosts()
+   
+  }, []);
+
+  console.log(posts)
+
+  return (
+    <div className="app">
+      <div className="app-container">
+        <h1 className="app-header">Posts</h1>
+
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2 className="post-title">{post.title}</h2>
+            <p className="post-content">{post.body}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AxiosComponents;
