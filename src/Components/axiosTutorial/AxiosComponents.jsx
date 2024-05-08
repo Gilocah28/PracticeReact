3b531b59dc702655bd2bf1e3f0ccc6cd7ffb5d4e
Gibermar Regalado <gberxx@gmail.com>
2024-05-08 15:48:29 +0800

    Axios CRUD

--- src/Components/axiosTutorial/AxiosComponents.jsx
+++ src/Components/axiosTutorial/AxiosComponents.jsx
@@ -1,29 +1,75 @@
 import React, { useEffect, useState } from "react";
-import { fetchPosts } from "../../service/apiService";
+import {
+  fetchPosts,
+  createPost,
+  updatePost,
+  deletePost,
+} from "../../service/apiService";
 
 const AxiosComponents = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const getPosts = async () => {
-        const postData = await fetchPosts();
-        setPosts(postData);
-    }
-    getPosts()
-   
+      const postData = await fetchPosts();
+      setPosts(postData);
+    };
+    getPosts();
   }, []);
 
-  console.log(posts)
+  const handleUpdatePost = async (id) => {
+    const updatedPostData = {
+      title: "Updated Post",
+      body: `This is Updated Post ${Date.now()}`,
+      userId: 1,
+    };
+
+    const updatedPost = await updatePost(id, updatedPostData);
+    setPosts(posts.map((p) => (p.id === id ? updatedPost : p)));
+  };
+
+  const handleDeletePost = async (id) => {
+    await deletePost(id);
+    setPosts(posts.filter((p) => p.id !== id));
+  };
+
+  const handleCreatePost = async () => {
+    const newPost = {
+      title: "New Post",
+      body: `This is new Post. ${Date.now()}`,
+      userId: 1,
+    };
+
+    const post = await createPost(newPost);
+    setPosts([post,...posts]);
+  };
 
   return (
     <div className="app">
       <div className="app-container">
         <h1 className="app-header">Posts</h1>
+        <button className="btn btn-success" onClick={handleCreatePost}>
+          Create
+        </button>
 
         {posts.map((post) => (
           <div className="post" key={post.id}>
             <h2 className="post-title">{post.title}</h2>
             <p className="post-content">{post.body}</p>
+
+            <button
+              className="btn btn-primary"
+              onClick={() => handleUpdatePost(post.id)}
+            >
+              Update
+            </button>
+
+            <button
+              className="btn btn-danger"
+              onClick={() => handleDeletePost(post.id)}
+            >
+              Delete
+            </button>
           </div>
         ))}
       </div>
--- src/service/apiService.js
+++ src/service/apiService.js
@@ -2,11 +2,41 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-//get post requests
-
+// get post requests
 export const fetchPosts = async () => {
   try {
     const { data } = await axios.get(`${API_URL}?_limit=10`);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Update Post
+export const updatePost = async (id, updatedPost) => {
+  try {
+    const { data } = await axios.put(`${API_URL}/${id}`, updatedPost);
+    return data;
+  } catch (error) {
+    console.error("Error updating post:", error);
+    throw new Error("Failed to update post");
+  }
+};
+
+// Delete Post
+export const deletePost = async (id) => {
+  try {
+    await axios.delete(`${API_URL}/${id}`);
+    return id;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Adding Post
+export const createPost = async (newPost) => {
+  try {
+    const { data } = await axios.post(API_URL, newPost);
     return data;
   } catch (error) {
     console.log(error);
